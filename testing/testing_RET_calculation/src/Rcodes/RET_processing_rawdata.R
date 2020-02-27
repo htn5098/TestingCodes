854e6384743e6f3958e9c6a3496eb5a3fb930da9
htn5098 <htn5098@psu.edu>
2020-02-27 18:16:39 -0500

    Setting up repository for tracking work

---
+++ testing/testing_RET_calculation/src/Rcodes/RET_processing_rawdata.R
@@ -0,0 +1,57 @@
+args = commandArgs(trailingOnly=T)
+filename=args[1]
+var=args[2]
+period=args[3]
+
+.libPaths("/storage/home/htn5098/local_lib/R35") # needed for calling packages
+.libPaths()
+
+library(ncdf4)
+library(data.table)
+library(foreach)
+library(doParallel)
+library(parallel)
+
+# Registering cores for parallel processing
+no_cores <- detectCores() #24 cores per node - enough for parallel processing
+#print(no_cores)
+cl <- makeCluster(no_cores)
+registerDoParallel(cl)
+
+# Reading the .nc file 
+print("Reading nc file")
+nc.file <- nc_open(filename) #
+
+# Reading supporting files
+print("Reading supporting files")
+coord_se <- read.table('./data/SDGrid0125sort.txt', sep = ',', header = T) # File contains information about grid index
+indx = sort(unique(coord_se$Grid)) # index numbers of grids in the study area
+
+# Extracting data from array to matrix
+# time <- seq.Date(as.Date('1979-01-01'), as.Date('2016-12-31'), by ='days')
+print("Extracting data")
+nc.var <- ncvar_get(nc.file, varid = var) # variable extracted in array format (lon,lat,day)
+nc.att <- ncatt_get(nc.file,varid=var)
+print(nc.att$unit)
+dim <- dim(nc.var)
+var.matrix <- aperm(nc.var, c(3,2,1)) # rearranging the dimensions of the array to (day,lat,lon)
+dim(var.matrix) <- c(dim[3],dim[2]*dim[1]) # turning the array into a 2d matrix (day,grid)
+var.matrix.sa <- var.matrix[,indx] # selecting only gridcells within the study area
+colnames(var.matrix.sa) <- as.character(indx)
+indx_NA <- which(colSums(is.na(var.matrix.sa)) != 0) # finding grids with NA's
+var.matrix.sa[,indx_NA] <- 0 # eliminating no data grids
+print("Data extraction complete")
+
+# Writing .csv file for each grid cells
+print("Writing data files")
+#setwd("./data/interim") #setting working directory to a new path resets the library paths, preventing the workers to load the package doParralel
+clusterExport(cl,list('var.matrix.sa','period','var')) #list('var.matrix.sa') expporting data into clusters for parallel processing
+foreach(i = 1:10) %dopar% { #ncol(var.matrix.sa)
+  outfile=data.frame(var.matrix.sa[,i])
+  grid=colnames(var.matrix.sa)[i]
+  outfilename=paste0('./data/interim/UW_',period,'_',var,'_',grid,'.csv')
+  print(outfilename)
+  read.csv(outfile,outfilename,row.names=F,col.names=F)
+}
+print("Completed")
+stopCluster(cl)
---
+++ testing/testing_RET_calculation/src/Rcodes/testing_install.R
@@ -0,0 +1,19 @@
+# "https://cran.revolutionanalytics.com"
+
+print(getOption("repos"))
+
+repos="https://cran.revolutionanalytics.com"
+lib="/storage/home/htn5098/local_lib/R35"
+
+
+#install.packages("ncdf4",repos=repos,lib =lib)
+#install.packages("Evapotranspiration",repos=repos,lib =lib)
+#install.packages("data.table",repos=repos,lib =lib)
+install.packages("dplyr",repos=repos,lib=lib)
+install.packages("tidyverse",repos=repos,lib=lib)
+install.packages("RODBC",repos=repos,lib=lib)
+
+#install.packages("foreach",repos=repos,lib=lib)
+#install.packages("doParallel",repos=repos,lib=lib)
+
+/storage/home/htn5098/work/DataAnalysis/data/raw/UW_clim/control
